e40007b532bc6d73bc327bb28ca1081d791f40fb
Oh-Bobcat <ohbobcat@yahoo.com>
2025-11-19 00:05:52 -0800

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const items = [
     url: "https://us.checkout.battle.net/shop/en/checkout/buy/1078296",
     region: "US",
     event: "Shop Exclusive",
-    notes: "Legendary Ashe skin not visible in the in-game Hero Gallery. Direct shop checkout.",
+    notes: "Sometimes available in the Shop.",
     priceCoins: 1900,
     priceLabel: "1,900 Coins",
     tags: ["Shop-only", "Example"],
